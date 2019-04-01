3c8d9bd239845527d41276f2c71cd590fe5481d9
Hanaasagi <ambiguous404@gmail.com>
2019-04-01 22:15:54 +0900

    add cli test

--- pymem/cli.py
+++ pymem/cli.py
@@ -1,3 +1,4 @@
+import sys
 import json
 import distutils.spawn
 
@@ -10,7 +11,19 @@ from .debugger import GDBDebugger
 from .debugger import BaseDebugger
 from .debugger import LLDBDebugger
 
+from typing import Any
+from typing import Dict
 from typing import Type
+
+
+def get_objects(debugger: BaseDebugger) -> Dict[Any, Any]:
+    code = pkg_resources.resource_string(__name__, "snippets/objects.py")
+    return debugger.debug_with(code.decode())
+
+
+def get_garbages(debugger: BaseDebugger) -> Dict[Any, Any]:
+    code = pkg_resources.resource_string(__name__, "snippets/garbages.py")
+    return debugger.debug_with(code.decode())
 
 
 @click.command()
@@ -24,14 +37,13 @@ from typing import Type
 )
 @click.option("-v", "--verbose", default=False, is_flag=True)
 def main(pid: int, debugger_kind: str, verbose: bool) -> None:
-    pretty = True
     if not check_process_exist(pid):
         click.echo(f"Process(pid={pid}) is not found.", err=True)
         click.get_current_context().exit(1)
 
     debugger_bin_path = distutils.spawn.find_executable(debugger_kind)
     if not debugger_bin_path:
-        click.echo(f"Could not find debugger in your bin path.", err=True)
+        click.echo("Could not find debugger in your bin path.", err=True)
         click.get_current_context().exit(1)
 
     if debugger_kind == "gdb":
@@ -43,16 +55,13 @@ def main(pid: int, debugger_kind: str, verbose: bool) -> None:
     debugger: BaseDebugger = debugger_cls(
         bin_path=debugger_bin_path, target_pid=pid, verbose=verbose
     )
-    code = pkg_resources.resource_string(__name__, "snippets/objects.py")
-    data["objects"] = debugger.debug_with(code.decode())
-    code = pkg_resources.resource_string(__name__, "snippets/garbages.py")
-    data["garbages"] = debugger.debug_with(code.decode())
+    data["objects"] = get_objects(debugger)
+    data["garbages"] = get_garbages(debugger)
     data["summary"] = get_summary(pid)
-    if pretty:
-        output = json.dumps(data, indent=4)
-    else:
-        output = json.dumps(data)
-    print(output)
+    output = json.dumps(data, indent=4)
+
+    sys.stdout.write(output)
+    sys.stdout.flush()
 
 
 if __name__ == "__main__":
---
+++ tests/test_cli.py
@@ -0,0 +1,67 @@
+import os
+import json
+import pytest
+from unittest.mock import patch
+from multiprocessing import Process, Event
+from pymem.cli import main
+from click.testing import CliRunner
+
+
+@pytest.fixture
+def cli():
+    return CliRunner()
+
+
+def test_invalid_pid(cli):
+    pid = os.getpid()
+    with patch("pymem.cli.check_process_exist") as mock_check_process_exist:
+        mock_check_process_exist.return_value = False
+        result = cli.invoke(main, [str(pid)])
+        assert result.exit_code == 1
+        assert f"Process(pid={pid}) is not found.\n" == result.output
+
+
+def test_no_debugger(cli):
+    pid = os.getpid()
+    with patch("distutils.spawn.find_executable") as mock_find_executable:
+        mock_find_executable.return_value = ""
+        result = cli.invoke(main, [str(pid)])
+        assert result.exit_code == 1
+        assert f"Could not find debugger in your bin path.\n" == result.output
+
+
+def test_print(cli):
+    def sleep_until_wake(e):
+        e.wait()
+
+    e = Event()
+    p = Process(target=sleep_until_wake, args=(e,))
+    p.start()
+    pid = p.pid
+    with patch("distutils.spawn.find_executable") as mock_find_executable:
+        mock_find_executable.return_value = "/usr/bin/gdb"
+        with patch("pymem.cli.get_objects") as mock_get_objects:
+            mock_get_objects.return_value = [
+                {
+                    "type": "<class 'abc.ABCMeta'>",
+                    "objects": 91,
+                    "total_size": "88.88 KiB",
+                }
+            ]
+            with patch("pymem.cli.get_garbages") as mock_get_garbages:
+                mock_get_garbages.return_value = {"count": 0, "objects": []}
+                result = cli.invoke(main, [str(pid)])
+                # make sure subprocess exit, before throwing AssertionError.
+                e.set()
+                p.join()
+                assert result.exit_code == 0
+                data = json.loads(result.output)
+                assert data["objects"] == [
+                    {
+                        "type": "<class 'abc.ABCMeta'>",
+                        "objects": 91,
+                        "total_size": "88.88 KiB",
+                    }
+                ]
+                assert data["garbages"] == {"count": 0, "objects": []}
+                assert "summary" in data
